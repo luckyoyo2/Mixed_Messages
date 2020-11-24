5dd6b2350edca1302180e89167d1feeb64b728d3
luckyoyo2 <yoann.diep@hotmail.fr>
2020-11-24 17:14:49 +0100

    Added the message variable

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,2 +1,6 @@
 // This project will output a random message when prompted by a user
 
+const you = {
+    verb: ['will', 'are', 'can'],
+    adjective: [['fall in love', 'die', 'succeed'], ['happy', 'sad', 'optimistic', 'pessimistic'], ['do it', 'be who you are']]
+} 
