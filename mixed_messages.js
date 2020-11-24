a0dbd6971548cde705ee611a0151d6fff6efdff7
luckyoyo2 <yoann.diep@hotmail.fr>
2020-11-24 17:25:56 +0100

    Displayed a message

--- mixed_messages.js
+++ mixed_messages.js
@@ -2,5 +2,10 @@
 
 const you = {
     verb: ['will', 'are', 'can'],
-    adjective: [['fall in love', 'die', 'succeed'], ['happy', 'sad', 'optimistic', 'pessimistic'], ['do it', 'be who you are']]
+    adjective: [['fall in love', 'die', 'succeed'], ['happy', 'sad', 'optimistic'], ['do it', 'be who you are', 'be successful']]
 } 
+
+const randomNumber = Math.floor(Math.random() * 3);
+const randomNumber2 = Math.floor(Math.random() * 3);
+
+console.log(`You ${you.verb[randomNumber]} ${you.adjective[randomNumber][randomNumber2]}`);
